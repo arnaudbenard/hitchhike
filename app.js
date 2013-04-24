54596245f79e9b6ab5a8eb18f0b913577fdf2248
Arnaud Benard <arnaudbenard13@gmail.com>
2013-04-24 16:09:34 -0700

    installed foundation and phantomjs

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ models.examples = require('./models/example')(app.mongoose).model;
 
 require('./routes')(app, models);
 
+var phantom = require('phantom');
+phantom.create(function(ph) {
+  return ph.createPage(function(page) {
+    return page.open("http://www.google.com", function(status) {
+      console.log("opened google? ", status);
+      return page.evaluate((function() {
+        return document.title;
+      }), function(result) {
+        console.log('Page title is ' + result);
+        return ph.exit();
+      });
+    });
+  });
+});
+
 app.listen(process.env.PORT || 3000);
+
